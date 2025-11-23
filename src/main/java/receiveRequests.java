7620cb53675caeb06c8de58e3ac2a727f038f462
Nickolas D'Alimonte <ND135@myscc.ca>
2025-11-23 14:45:36 -0500

    restructuring

---
+++ src/main/java/receiveRequests.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class receiveRequests {
+}
