fb75ec844521a8befd97e56bae838a07404204dd
Nickolas D'Alimonte <ND135@myscc.ca>
2025-11-29 18:50:51 -0500

    auth Listener: Got content length Planned for malformed header got POST body

--- src/main/java/Server/AuthListener.java
+++ src/main/java/Server/AuthListener.java
@@ -4,6 +4,7 @@ package Server;
 //Bad logic for determining login/creation conditions (maybe use a different listener & port for creation/login???)
 //Potentially doesn't read the entire message if it's longer than 1024 bytes. Not sure if this is a problem?
 
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -19,23 +20,47 @@ public class AuthListener extends PortListener{
 
     @Override
     public void handleClient(Socket socket) throws IOException {
+        OutputStream os = socket.getOutputStream();
+        InputStream is = socket.getInputStream();
+        BufferedReader br =  new BufferedReader(new InputStreamReader(is));
+        int contentLength = 0;
+        String contentLine;
+        String response =
+                "HTTP/1.1 303 See Other\r\n" +
+                        "Location: https://Nicksproject.ca\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n";
         System.out.println("Message Received on Port: "+socket.getLocalPort());
 
-        InputStream is = socket.getInputStream();
+        while((contentLine = br.readLine()) != null && !contentLine.isEmpty()){
 
-        byte[] b = new byte[1024];
+            if(contentLine.toLowerCase().startsWith("content-length:")){
+                String header = contentLine.trim().split(":")[1].trim();
+                if(header.chars().allMatch(Character::isDigit)){
+                    contentLength = Integer.parseInt(header);
+                }
+                else{
+                    int digitLocation = 0;
+                    for(int i = 0; i < header.length(); i++){
+                        if (!Character.isDigit(header.charAt(i))){
+                            digitLocation = i;
+                            break;
+                        }
+                    }
 
-        String message = new String(b, 0, is.read(b), StandardCharsets.UTF_8);
-
-        if(message.contains("uName") && message.contains("email")){
-            //create account
-            System.out.println("Account created");
-
+                    contentLength = Integer.parseInt(header.substring(0,digitLocation));
+                }
+            }
         }
 
-        else if(message.contains(username) && message.contains(password)){
-            System.out.println("Logged in");
-        }
+        char[] bodyChars = new char[contentLength];
+        br.read(bodyChars, 0, contentLength);
+
+        String body = new String(bodyChars);
+        System.out.println(body);
+
+        os.write(response.getBytes(StandardCharsets.UTF_8));
 
 
     }
